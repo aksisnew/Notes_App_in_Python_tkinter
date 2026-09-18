fa7236aaba544a1e321f014f152ab1cbe59b7622
Akshun A <203590953+aksisnew@users.noreply.github.com>
2026-09-18 17:17:44 +0530

    Update variables.py

--- src/variables.py
+++ src/variables.py
@@ -1,1 +1,9 @@
+class PurpleBerryState:
+    # Strictly enforced palette
+    bg_pink = "#f3d9ee"       # Faded pinkish-purple
+    fg_purple = "#3a205e"     # Deep dark purple
+    accent_red = "#d97eb3"    # Faded red
+    base_blue = "#8c9ce6"     # Faded blue
 
+    # Stashing shared app data
+    active_filepaths = {}
